c049192fce179731c1fb86cc0a06d91095fe8544
DivjyotSingh <singhdivjyot911@gmail.com>
2022-06-09 09:43:37 +0530

    Area Completed

--- src/pages/Charts/Area.js
+++ src/pages/Charts/Area.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import {
+  ChartComponent,
+  DateTime,
+  Legend,
+  SplineAreaSeries,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Tooltip,
+  Inject,
+} from "@syncfusion/ej2-react-charts";
+import React from "react";
+import Header from "../../components/Header";
+import { useStateContext } from "../../contexts/ContextProvider";
+import {
+  areaCustomSeries,
+  areaPrimaryXAxis,
+  areaPrimaryYAxis,
+  lineCustomSeries,
+  LinePrimaryXAxis,
+  LinePrimaryYAxis,
+} from "../../data/dummy";
 
 const Area = () => {
+  const { currentMode } = useStateContext();
   return (
-    <div>Area</div>
-  )
-}
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category={"Charts"} title="" />
+      <ChartComponent
+        height="420px"
+        primaryXAxis={areaPrimaryXAxis}
+        primaryYAxis={areaPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+        background={currentMode === "Dark" ? "#33373E" : "#fff"}
+      >
+        <Inject services={[SplineAreaSeries, DateTime, Legend, Tooltip]} />
+        <SeriesCollectionDirective>
+          {areaCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
+};
 
-export default Area
+export default Area;
