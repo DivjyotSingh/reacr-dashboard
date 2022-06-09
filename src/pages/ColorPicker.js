3f340977b0263b666bd827b12a8215449da9a330
DivjyotSingh <singhdivjyot911@gmail.com>
2022-06-09 08:08:13 +0530

    Color-Picker App Completed

--- src/pages/ColorPicker.js
+++ src/pages/ColorPicker.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import { ColorPickerComponent } from "@syncfusion/ej2-react-inputs";
+import React from "react";
+import Header from "../components/Header";
 
+const change = (args) => {
+  document.getElementById("preview").style.backgroundColor =
+    args.currentValue.hex;
+};
 const ColorPicker = () => {
   return (
-    <div>ColorPicker</div>
-  )
-}
+    <div className="m=2 md:m-10 mt-24 p-2 rounded-3xl bg-white md:p-10">
+      <Header category={"App"} title="Color Picker" />
+      <div className="text-center">
+        <div id="preview" />
+        <div className="flex justify-center items-center gap-20 flex-wrap">
+          <p className="text-2xl font-semibold mt-2 mb-4">Inline Palette</p>
+          <ColorPickerComponent
+            id="inline-palette"
+            mode="Palette"
+            modeSwitcher={false}
+            inline
+            showButtons={false}
+            change={change}
+          />
+        </div>
+        <div className="flex justify-center items-center gap-20 flex-wrap">
+          <p className="text-2xl font-semibold mt-2 mb-4">Inline Palette</p>
+          <ColorPickerComponent
+            id="inline-palette"
+            mode="Picker"
+            modeSwitcher={false}
+            inline
+            showButtons={false}
+            change={change}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ColorPicker
+export default ColorPicker;
--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import {
+  RichTextEditor,
+  RichTextEditorComponent,
+  Inject,
+  HtmlEditor,
+  Toolbar,
+  Image,
+  Link,
+  QuickToolbar,
+} from "@syncfusion/ej2-react-richtexteditor";
+import React from "react";
+import Header from "../components/Header";
+import { EditorData } from "../data/dummy";
 
 const Editor = () => {
   return (
-    <div>Editor</div>
-  )
-}
+    <div className="m=2 md:m-10 mt-24 p-2 rounded-3xl bg-white md:p-10">
+      <Header category={"App"} title="Editor" />
+      <RichTextEditorComponent>
+        <EditorData />
+        <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
+      </RichTextEditorComponent>
+    </div>
+  );
+};
 
-export default Editor
+export default Editor;
